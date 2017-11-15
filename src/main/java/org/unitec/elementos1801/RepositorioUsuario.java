099cc40e2c94bddbdf4207b0eabb077aa1fb89d2
YOO <YOO@192.168.1.75>
2017-11-15 10:00:07 -0600

    Creación de Repositorios Usuario y Dirección.

---
+++ src/main/java/org/unitec/elementos1801/RepositorioUsuario.java
@@ -0,0 +1,11 @@
+
+package org.unitec.elementos1801;
+
+import org.springframework.data.repository.CrudRepository;
+
+/*
+ * @author YOO
+ */
+public interface RepositorioUsuario extends CrudRepository<Usuario, Long>{
+    
+}
